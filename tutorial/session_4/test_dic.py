644c0e6c8392bcf8217d61ea993e23fefcac6a20
Hamzeh <h.qorbani7@gmail.com>
2025-07-13 12:28:43 +0330

    dic guide

---
+++ tutorial/session_4/test_dic.py
@@ -0,0 +1,7 @@
+strategies = {
+                'poor' : 'Comprehensive review + Practice with basic questions',
+                'average' : 'Practice questions + Review of key points',
+                'good' : 'Quick review of important concepts'
+            }
+level = 'average'
+print(strategies[level])
